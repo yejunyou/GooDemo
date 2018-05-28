1b1276d5c332779492232d7c6d605838227c4f56
yejunyou <ye_junyou@163.com>
2018-05-28 16:25:11 +0800

    Initial Commit

---
+++ GooDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GooDemo
+//
+//  Created by yejunyou on 2018/5/28.
+//  Copyright © 2018年 futureversion. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
